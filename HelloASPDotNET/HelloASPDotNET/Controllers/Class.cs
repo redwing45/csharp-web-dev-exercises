1195d290dfc5d6f92b6ddd7608c719e1781de60a
Ashley <adlenarich@gmail.com>
2023-10-08 12:46:57 -0500

    Controllers, methods and forms Oh my

---
+++ HelloASPDotNET/HelloASPDotNET/Controllers/Class.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<select name='language'><option value='english' selected>English</option>" +
+                "<option value='spanish'>Spanish</spanish>" +
+                "<option value='vietnamese'>Vietnamese</option>" +
+                "<option value='french'>French</option></select>" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+
+            return Content(html, "text/html");
+
+        }
+        public static CreateMessage(string name, string language)
+        {
+            string hello = "";
+            if (language = "French")
+            {
+                else if(language = Spanish)
+                {
+
+                }else
+                {
+                    heloo = "Hello";
+                }
+            }
+            return hello + name;
+        }
+    }
+}
---
+++ HelloASPDotNet1/HelloASPDotNet1/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNet1
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+
+        //GET: /helloworld/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //string html = "<h1>" + "Hello World!" + "</h1>";
+            string html = "<form method = 'post' action = '/helloworld/welcome'>" +
+                "input type = 'text' name = 'name'/>" +
+                "input type = 'submit' value = 'Greet me!' /> " +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        //POST:/helloworld/welcome
+        [HttpPost("/welcome")]
+        [HttpGet("welcome/{name?}")]
+        //[Route("/helloworld/welcome/{name}")]
+        //[Route("/helloworld/welcome/{name?}")]
+         public IActionResult Welcome(string name= "World") 
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
